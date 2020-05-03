3c507c173c58a0dd45d0a817bfb0b3716a77b3c9
Mateus de Oliveira <mateus.olvr@gmail.com>
2020-05-03 18:21:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace haplon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var folderPath = "events/";
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.txt"))
+                {
+                    try
+                    {
+                        string contents = File.ReadAllText(file);
+                        Console.WriteLine(contents);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("The file could not be read:");
+                        Console.WriteLine(e.Message);
+                    }
+
+
+                }                          
+    }
+}
+}
